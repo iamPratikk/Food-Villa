005ab3d351d646d18ae553502d1b6fcdc17e9979
pratik <pratikpriyadarshi1998@gmail.com>
2023-02-16 23:44:14 +0530

    fix cors

--- src/components/Body.js
+++ src/components/Body.js
@@ -36,7 +36,7 @@ const Body = () => {
 
   async function getData() {
     let data = await fetch(
-      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.591108571917196&lng=77.30704950738556&page_type=DESKTOP_WEB_LISTING`
+      `https://foodvilla-nr.netlify.app/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.591108571917196&lng=77.30704950738556&page_type=DESKTOP_WEB_LISTING`
     );
 
     let res = await data.json();
